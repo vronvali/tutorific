d81356429ce32c0c106b459873fbb44727bb0bff
Travis <vronvali@cesine.ca>
2015-07-21 16:05:37 -0400

    changed it so you get a message when you fail
    Furthered it by making the message cute

--- gulp/unit-tests.js
+++ gulp/unit-tests.js
@@ -6,13 +6,13 @@ var conf = require('./conf');
 
 var karma = require('karma');
 
-function runTests (singleRun, done) {
+function runTests(singleRun, done) {
   karma.server.start({
     configFile: path.join(__dirname, '/../karma.conf.js'),
     singleRun: singleRun,
     autoWatch: !singleRun
-  }, function() {
-    done();
+  }, function(failCount) {
+    done(failCount ? new Error("Failed " + failCount + " tests.") : null);
   });
 }
 
